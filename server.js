06adaf6e719a90e16f060be20a53475884f27416
Matthew Foulks <Matt@notelek.com>
2013-08-21 20:04:34 -0400

    added formatting, and uptime under debug

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ function start(route, debug, port) {
 	if(debug == 1){
 		console.log("Request for " + pathname + " received.");
 	}
-    route(pathname, debug);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
+		route(pathname, debug);
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
 
-  http.createServer(onRequest).listen(port);
-  console.log("Server has started.");
-  setInterval(function(){uptime++;}, 1000);
-  setInterval(function(){console.log(uptime);}, 1000);
+	http.createServer(onRequest).listen(port);
+	console.log("Server has started.");
+	if(debug == 1){
+		setInterval(function(){uptime++;}, 1000);
+		setInterval(function(){console.log("Uptime (seconds): "+uptime);}, 60000);
+	}
 }
 
 exports.start = start;
