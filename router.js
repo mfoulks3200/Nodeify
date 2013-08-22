98a1635cc62d3375f772bf2540f9d15b8d7bdb0f
Matthew Foulks <Matt@notelek.com>
2013-08-21 20:05:29 -0400

    removed old routing code

--- router.js
+++ router.js
@@ -2,7 +2,6 @@ function route(pathname, debug) {
 	if(debug == 1){
 		console.log("Routing request for C:/nodejswebserver/www" + pathname);
 	}
-	return fs.readFile('C:/nodejswebserver/www/'+pathname);
 }
 
 exports.route = route;
