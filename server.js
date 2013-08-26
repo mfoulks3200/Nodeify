46261b25e5b162f9557b3ce0eaeec1fdb51f3c32
Matthew Foulks <Matt@notelek.com>
2013-08-25 20:21:27 -0400

    added all colors

--- server.js
+++ server.js
@@ -17,12 +17,36 @@ function getTime(){
 	return h+":"+m+":"+s+" "+mo+"/"+d+"/"+y;
 }
 
-function log(message){
+function log(message, color){
 	var red, blue, reset;
 	red   = '\u001b[31m';
-	blue  = '\u001b[36m';
-	reset = '\u001b[0m';
-	console.log(blue + "["+getTime()+"] "+ reset +message);
+	green  = '\u001b[32m';
+	yellow   = '\u001b[33m';
+	blue  = '\u001b[34m';
+	magenta   = '\u001b[35m';
+	cyan  = '\u001b[36m';
+	white   = '\u001b[37m';
+	if(color == "red"){
+		console.log(cyan + "["+getTime()+"] "+ red +message);
+	}
+	if(color == "green"){
+		console.log(cyan + "["+getTime()+"] "+ green +message);
+	}
+	if(color == "yellow"){
+		console.log(cyan + "["+getTime()+"] "+ yellow +message);
+	}
+	if(color == "blue"){
+		console.log(cyan + "["+getTime()+"] "+ blue +message);
+	}
+	if(color == "magenta"){
+		console.log(cyan + "["+getTime()+"] "+ magenta +message);
+	}
+	if(color == "cyan"){
+		console.log(cyan + "["+getTime()+"] "+ cyan +message);
+	}
+	if(color == "white"){
+		console.log(cyan + "["+getTime()+"] "+ white +message);
+	}
 	fs.appendFile("log.txt", "["+getTime()+"] "+message+"\r\n");
 }
 
@@ -34,10 +58,10 @@ function checkTime(i){
 }
 
 function start(debug, port) {
-	log("Server has started.");
+	log("Server has started.", "white");
   function onRequest(request, response) {
 		if(debug == 1){
-			log("Request for " +request+ " received.");
+			log("Request for " +request+ " received.", "white");
 		}
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("Hello World");
@@ -45,10 +69,10 @@ function start(debug, port) {
 	}
 
 	http.createServer(onRequest).listen(port);
-	log("Started Listening");
+	log("Started Listening", "white");
 	if(debug == 1){
 		setInterval(function(){uptime++;}, 36000000);
-		setInterval(function(){log("Uptime (Hours): "+uptime);}, 36000000);
+		setInterval(function(){log("Uptime (Hours): "+uptime, "white");}, 36000000);
 	}
 }
 
