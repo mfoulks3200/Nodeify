8b837f2ab7ad10c8334e1b049ff5b84a9c2d68f4
Matthew Foulks <Matt@notelek.com>
2013-08-27 20:52:42 -0400

    added config file

---
+++ config.js
@@ -0,0 +1,6 @@
+//This is the nodejs webserver configuration file, edit the values to configure your server
+//Credits:
+//Webserver written by: Matthew Foulks (http://www.notelek.com)
+//NodeJS by: Joyent Inc. (http://nodejs.org/)
+
+var port = 80;
